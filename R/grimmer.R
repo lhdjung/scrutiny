7cd9ce8e3e5f5d90218e1a84cd20029d318882b3
Lukas Jung <jung-lukas@gmx.net>
2022-01-10 14:38:52 +0100

    some new ideas for variances

--- R/grimmer.R
+++ R/grimmer.R
@@ -1,16 +1,43 @@
+
+# # [DOESN'T WORK] Anaya's variance function, translated into R:
+# variance <- function(data) {
+#   u <- sum(data) / length(data)
 #
+#   ((data - u) ^ 2) / length(data)
+# }
 #
-# x <- 3
-# n <- 2
-#
-# digits <- 2
-#
-# combinations <- combine_elements(1:x, n)
-# combinations
-#
-# func_for_combination_rows <- function(combinations, digits)
+# # Irrelevant:
+# func_for_combination_rows <- function(combinations, digits) {}
 #
 # if (any(combinations == n - 1)) {}
 #
 #
 # purrr::pmap(combinations, )
+
+
+# # My own NSE variance function:
+# variance_dots <- function(...) {
+#   numbers <- rlang::enexprs(...)
+#   numbers <- purrr::map_dbl(numbers, rlang::eval_bare)
+#
+#   var(numbers)
+# }
+#
+# # My own example:
+# data <- combine_elements(0:4, 5)
+#
+#
+# # Are these the GRIMMER variances?
+# variances <- data %>%
+#   purrr::pmap_dbl(variance_dots)
+#
+#
+# # An *attempt* at a GRIMMER function to generate variances:
+# grimmer_variances <- function(x, n) {
+#   combine_elements(x, n) %>%
+#     purrr::pmap_dbl(variance_dots) %>%
+#     sort() %>%
+#     unique()
+# }
+
+
