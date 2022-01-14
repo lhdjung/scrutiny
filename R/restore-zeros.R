c24ad8d88668995022ecd45428e3c3f99a1ac9dd
Lukas Jung <jung-lukas@gmx.net>
2022-01-14 10:03:15 +0100

    rewrote function as `sprintf()` wrapper

--- R/restore-zeros.R
+++ R/restore-zeros.R
@@ -61,25 +61,17 @@
 
 restore_zeros <- function(x, width = NULL, sep = "\\.") {
 
-  # # To get trailing zeros, all elements need to be strings:
-  # x <- as.character(x)
-
   # Make sure no whitespace (from values that already were strings) is factored
   # into the count:
   x <- stringr::str_trim(x)
 
-  # Count characters of the integer and mantissa parts via internal helper
-  # functions:
-  # width_integer <- width_integer(x)
-  # width_mantissa <- width_mantissa(x)
-
+  # Count characters of the mantissa part:
   parts <- stringr::str_split_fixed(x, sep, n = 2)
-  width_integer <- stringr::str_length(parts[, 1])
   width_mantissa <- stringr::str_length(parts[, 2])
 
   # Determine the maximal width to which the mantissas should be padded in
   # accordance with the `width` argument, the default of which, `NULL`, makes
-  # the function go for the maximal length of already-present mantissas:
+  # the function go by the maximal length of already-present mantissas:
   if (is.null(width)) {
     if (length(x) == 1) {
       rlang::warn(glue::glue(
@@ -97,30 +89,12 @@ restore_zeros <- function(x, width = NULL, sep = "\\.") {
     width_target <- width
   }
 
-  # The number of trailing zeros to be added takes the respective lengths of
-  # three variables into account: the integer part (via an internal helper
-  # function), the decimal point (i.e., 1), and the target mantissa length
-  # (determined above). Note that the integer part is only relevant for being
-  # balanced against, precisely so that it ultimately plays no role. Also note
-  # that `"i"` and `"d"` are just placeholders here, used to make sure there is
-  # exactly one `sep` substring (default is a decimal point) in each resulting
-  # string value...
-  out <- stringr::str_pad(x, width = width_integer + 1 + width_target,
-                          side = "right", pad = "i") %>%
-    stringr::str_replace(    "i", sep) %>%
-    stringr::str_replace_all("i", "0") %>%
-    stringr::str_replace(    sep, "d") %>%
-    stringr::str_replace_all(sep, "0") %>%
-    stringr::str_replace(    "d", sep)
+  # Assemble the formatting regex, determined by `width_target` -- the desired
+  # number of decimal places to which the `x` values should be padded:
+  format <- paste0("%.", width_target, "f")
 
-  # ...unless a string has no decimal places, in which case the `sep` substring
-  # at the end of the value would be superfluous. It is removed before the
-  # output is returned:
-  dplyr::if_else(
-    stringr::str_detect(out, paste0(sep, "$")),  # used to have: "\\.$"
-    stringr::str_remove(out, sep),
-    out
-  )
+  # Pad `x` with the correct amount of trailing zeros:
+  sprintf(format, as.numeric(x))
 
 }
 
