6e8f3df4a6ca102c25bf3c0bacaacd3fcfe45425
Lukas Jung <jung-lukas@gmx.net>
2022-04-26 21:12:15 +0200

    new `audit()` methods for manufactured functions

---
+++ R/method-debit-map-disperse.R
@@ -0,0 +1,8 @@
+
+
+#' @include audit.R reverse-map-disperse.R
+#' @export
+
+audit.scr_debit_map_disperse <- function(data) {
+  summarize_map_disperse(data)
+}
---
+++ R/method-grim-map-disperse.R
@@ -0,0 +1,9 @@
+
+
+#' @include audit.R reverse-map-disperse.R
+#' @export
+
+
+audit.scr_grim_map_disperse <- function(data) {
+  summarize_map_disperse(data)
+}
