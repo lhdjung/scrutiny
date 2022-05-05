80e19a8b980d8db658c26ce53afdaa19a01aaef6
Lukas Jung <jung-lukas@gmx.net>
2022-05-05 23:00:24 +0200

    refactoring, cleanup

--- R/function-map-seq.R
+++ R/function-map-seq.R
@@ -23,16 +23,6 @@ function_map_seq <- function(.fun, .name_test, .name_class = NULL,
 # Helper used within the `*_proto()` function below; not exported:
 seq_disperse_df <- function(x, dispersion, include_reported = TRUE) {
 
-  # length_dispersed_seq <- (2 * length(dispersion)) - 1
-  # length_wing <- length(dispersion) - 1
-  #
-  # seq_distance_df(
-  #   .from = x,
-  #   .length_out = length_dispersed_seq,
-  #   .offset_from = -length_wing,
-  #   .string_output = is.character(x)
-  # )
-
   digits <- max(decimal_places(x))
   p10 <- 1 / (10 ^ digits)
   dispersion <- dispersion * p10
@@ -61,28 +51,31 @@ seq_disperse_df <- function(x, dispersion, include_reported = TRUE) {
 # # Example data:
 # data <- grim_map(pigs1)
 # fun  <- grim_map
-# var  <- "x"    # `var` is the variable to be varied, i.e., dispersed.
+# var  <- "n"    # `var` is the variable to be varied, i.e., dispersed.
 # dispersion <- 1:5
 # include_reported <- TRUE
 
 
 function_map_seq_proto <- function(.fun = fun, .var = var,
-                                   .dispersion = dispersion, ...) {
+                                   .dispersion = dispersion,
+                                   .include_reported = include_reported, ...) {
 
   # The function factory returns this manufactured function:
-  function(data, fun = .fun, var = .var,
-                             dispersion = .dispersion, ...) {
+  function(data, fun = .fun, var = .var, dispersion = .dispersion,
+           include_reported = .include_reported, ...) {
 
-    list_var <- data %>%
-      dplyr::pull({{ var }}) %>%
-      purrr::map(
-        seq_disperse_df,
-        dispersion = dispersion,
-        include_reported = include_reported
-      )
+    data_var <- match(var, colnames(data))
+    data_var <- data[data_var]
+    data_var <- data_var[[1]]
 
-    nrow_list_var <- list_var %>%
-      purrr::map_int(nrow)
+    list_var <- purrr::map(
+      data_var,
+      seq_disperse_df,
+      dispersion = dispersion,
+      include_reported = include_reported
+    )
+
+    nrow_list_var <- purrr::map_int(list_var, nrow)
 
     ncol_index_var <- match(var, colnames(data))
     ncol_before_consistency <- match("consistency", colnames(data)) - 1
@@ -94,28 +87,13 @@ function_map_seq_proto <- function(.fun = fun, .var = var,
     # Short for "columns except (for the) last (one)":
     cols_el <- 1:length(cols_for_testing_names_without_var)
 
-    data_list_without_var <- data %>%
-      dplyr::select({{ cols_for_testing_names_without_var }}) %>%
-      dplyr::mutate(
-        nrow_list_var,
-        dplyr::across({{ cols_el }}, as.list),
-        dplyr::across({{ cols_el }}, ~ purrr::map2(., nrow_list_var, rep)),
-        nrow_list_var = NULL
-      )
-
-    # data_list_df_other_cols <- data_list_df[1:(ncol(data_list_df) - 1)]
-
-    # # This part isn't all that elegant, but I'm not sure what a more smooth
-    # # solution would look like. MUCH MORE SERVERELY, IT OVERFITS THE COLUMN
-    # # NAMES!!!
-    # data_list_df <- data_list_without_var %>%
-    #   dplyr::mutate(
-    #     list_var,
-    #     list_var = purrr::map2(list_var, n, dplyr::mutate),
-    #     list_var = purrr::map(list_var, dplyr::rename, n = `<int>`),
-    #     list_var = purrr::map2(list_var, items, dplyr::mutate),
-    #     list_var = purrr::map(list_var, dplyr::rename, items = `<int>`)
-    #   )
+    data_list_without_var <- dplyr::mutate(
+      data[cols_for_testing_names_without_var],
+      nrow_list_var,
+      dplyr::across({{ cols_el }}, as.list),
+      dplyr::across({{ cols_el }}, ~ purrr::map2(., nrow_list_var, rep)),
+      nrow_list_var = NULL
+    )
 
     # Prepare the data frames for testing:
     data_list_for_testing <- data_list_without_var %>%
