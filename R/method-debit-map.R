730d2ff0fc763ec785505e6fe8311fab800dc18a
Lukas Jung <jung-lukas@gmx.net>
2022-05-11 20:41:35 +0200

    refactoring

--- R/method-debit-map.R
+++ R/method-debit-map.R
@@ -7,10 +7,7 @@ audit.scr_debit_map <- function(data) {
   # Compute the summary values of interest --
 
   # 1. the number of DEBIT-inconsistent cases:
-  incons_cases <- data %>%
-    dplyr::filter(.data$consistency == FALSE) %>%
-    dplyr::count() %>%
-    as.numeric()
+  incons_cases <- nrow(data[!data$consistency, ])
 
   # 2. the total number of cases:
   all_cases <- nrow(data)
@@ -19,20 +16,19 @@ audit.scr_debit_map <- function(data) {
   incons_rate <- as.numeric(incons_cases / all_cases)
 
   # 4. the mean `x` value:
-  mean_x <- data %>%
-    dplyr::summarise(mean_x = mean(as.numeric(.data$x))) %>%
-    as.numeric()
+  mean_x <- data$x %>%
+    as.numeric() %>%
+    mean()
 
   # 5. the mean `sd` value:
-  mean_sd <- data %>%
-    dplyr::summarise(mean_sd = mean(as.numeric(.data$sd))) %>%
-    as.numeric()
+  mean_sd <- data$sd %>%
+    as.numeric() %>%
+    mean()
 
   # 6. the number of distinct `n` values:
-  distinct_n <- data %>%
-    dplyr::distinct(.data$n) %>%
-    nrow() %>%
-    as.numeric()
+  distinct_n <- data$n %>%
+    unique() %>%
+    length()
 
 
   # Finally, collect all of these values in a resulting tibble --
