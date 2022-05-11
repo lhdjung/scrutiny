c71d0de5b54863a9e7f815169a867ae0ba7f6624
Lukas Jung <jung-lukas@gmx.net>
2022-05-11 20:43:07 +0200

    renamed function, rewrote error message

--- R/manage-extra-cols.R
+++ R/manage-extra-cols.R
@@ -1,6 +1,6 @@
 
 
-check_extra_cols <- function(data, extra, other_cols) {
+manage_extra_cols <- function(data, extra, other_cols) {
 
   # Throw error if `extra` is a wrong string; i.e., if there is no extra column
   # by that name in `data`:
@@ -21,27 +21,45 @@ check_extra_cols <- function(data, extra, other_cols) {
   if (!is.infinite(extra) && is.numeric(extra) &&
       any(c(extra, length(extra)) > length(other_cols))) {
 
-    n_cols_info <- dplyr::case_when(
-      length(other_cols) == 0  ~ glue::glue("there aren't any extra columns"),
-      length(other_cols) == 1  ~ glue::glue("there's only 1 extra column"),
-      TRUE                     ~ glue::glue("there are only \\
-                                            {length(other_cols)} extra columns")
-    )
+    # Prepare error message:
+    if (length(other_cols) == 0) {
+      msg_n_cols <- "there aren't any extra columns"
+    } else if (length(other_cols) == 1) {
+      msg_n_cols <- "there's only 1 extra column"
+    } else {
+      msg_n_cols <- glue::glue(
+        "there are only {length(other_cols)} extra columns"
+      )
+    }
+
+    # msg_n_cols <- dplyr::case_when(
+    #   length(other_cols) == 0  ~ glue::glue("there aren't any extra columns"),
+    #   length(other_cols) == 1  ~ glue::glue("there's only 1 extra column"),
+    #   TRUE                     ~ glue::glue("there are only \\
+    #                                         {length(other_cols)} extra columns")
+    # )
 
     if (length(extra) == 1) {
       cli::cli_abort(c(
-      "`extra` was specified as {extra}, for extra column number {extra}.",
-      "x" = "This number is too high -- {n_cols_info} in `data`."
+      "`extra` was specified as `{extra}`, for extra column number {extra}.",
+      "x" = "This number is too high -- {msg_n_cols} in `data`."
       ))
     } else {
-      num_length_info <- dplyr::if_else(
-        length(other_cols) < extra[1],
-        "These numbers are",
-        "That last number is"
-      )
+
+      if (length(other_cols) < extra[1]) {
+        msg_num_length <- "These numbers are"
+      } else {
+        msg_num_length <- "That last number is"
+      }
+
+      # msg_num_length <- dplyr::if_else(
+      #   length(other_cols) < extra[1],
+      #   "These numbers are",
+      #   "That last number is"
+      # )
       cli::cli_abort(c(
       "`extra` was specified as `{rlang::enexprs(extra)}`.",
-      "x" = "{num_length_info} too high -- {n_cols_info} in `data`."
+      "x" = "{msg_num_length} too high -- {msg_n_cols} in `data`."
       ))
     }
   }
@@ -49,7 +67,7 @@ check_extra_cols <- function(data, extra, other_cols) {
 
   # Finally, make `other_cols` capture any and all extra columns, and return it:
   if (!is.infinite(extra) && length(other_cols) > 0) {
-    return(dplyr::select(other_cols, all_of(extra)))
+    return(dplyr::select(other_cols, dplyr::all_of(extra)))
   } else {
     return(other_cols)
   }
