673db4188583c487f4fa1a6b2e077b297f6020f6
Lukas Jung <jung-lukas@gmx.net>
2022-06-03 19:19:30 +0200

    cleanup

--- R/reverse-map-total-n.R
+++ R/reverse-map-total-n.R
@@ -65,13 +65,6 @@ reverse_map_total_n <- function(data) {
   # ones do need a correction:
   n_was_odd <- as.numeric(!n_was_even)
 
-  # # `n_was_odd` is `1` if the original, total `n` was odd, and `0` otherwise. In
-  # # this way, when `n_was_odd` is later added to `2 * n`, it completes the
-  # # restoration of the original total:
-  # n_was_odd <- data_reduced$n_change %>%
-  #   stringr::str_detect("n2") %>%
-  #   as.numeric()
-
   data_reduced <- data_reduced %>%
     dplyr::ungroup() %>%
     dplyr::mutate(
@@ -111,42 +104,4 @@ reverse_map_total_n <- function(data) {
 
   return(out)
 }
-
-
-
-# @rdname reverse_map_total_n
-# @export
-
-# summarize_map_total_n <- function(data) {
-#
-#   df_list <- split(data, data$case)
-#
-#   df_list_hits <- df_list %>%
-#     purrr::map(dplyr::filter, both_consistent)
-#
-#   hits_forth <- df_list_hits %>%
-#     purrr::map(dplyr::filter, dir == "forth") %>%
-#     purrr::map_int(nrow) %>%
-#     `/`(2)
-#
-#   hits_back <- df_list_hits %>%
-#     purrr::map(dplyr::filter, dir == "back") %>%
-#     purrr::map_int(nrow) %>%
-#     `/`(2)
-#
-#   hits_total <- hits_forth + hits_back
-#
-#   scenarios_total <- df_list %>%
-#     purrr::map_int(nrow) %>%
-#     `/`(2)
-#
-#   hit_rate <- hits_total / scenarios_total
-#
-#   data_rec <- reverse_map_total_n(data)
-#
-#   out <- data_rec %>%
-#     dplyr::mutate(hits_forth, hits_back, hits_total, scenarios_total, hit_rate)
-#
-#   return(out)
-# }
 
