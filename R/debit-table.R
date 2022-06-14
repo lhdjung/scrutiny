8e7bc1f9d80115c1ece3f992007794cacbf032a3
Lukas Jung <jung-lukas@gmx.net>
2022-06-14 15:53:41 +0200

    rewrite basic DEBIT implementation

--- R/debit-table.R
+++ R/debit-table.R
@@ -2,8 +2,7 @@
 
 # Helper to check input ranges (not exported) -----------------------------
 
-
-proto_debit_inputs <- function(input, type, symbol) {
+check_debit_inputs <- function(input, type, symbol) {
 
   # For all input values, check if they are between 0 and 1:
   input_in_range <- input %>%
@@ -12,12 +11,18 @@ proto_debit_inputs <- function(input, type, symbol) {
 
   # If at least one of the values is outside of that range, this will lead to an
   # error. First, the error message is prepared...
-  if (!all(input_in_range)) {
+  offenders <- input[!input_in_range]
 
-    offenders <- input[!input_in_range]
-    is_are <- dplyr::if_else(length(offenders) == 1, "is", "are")
+  if (length(offenders) > 0) {
+
+    if (length(offenders) == 1) {
+      msg_is_are <- "is"
+    } else {
+      msg_is_are <- "are"
+    }
 
     if (length(offenders) > 3) {
+      offenders_all <- offenders
       offenders <- offenders[1:3]
       msg_offenders <- ", starting with"
     } else {
@@ -26,22 +31,24 @@ proto_debit_inputs <- function(input, type, symbol) {
 
     # ...and second, the actual error is thrown:
     cli::cli_abort(c(
-      "DEBIT only works with binary summary data",
+      "DEBIT only works with binary summary data.",
       "!" = "Binary {type} (`{symbol}`) values must range from 0 to 1.",
-      "x" = "{length(input[!input_in_range])} out of {length(input)} \\
-      `{symbol}` values {is_are} not in that range{msg_offenders} {offenders}."
+      "x" = "{length(offenders_all)} out of {length(input)} \\
+      `{symbol}` values {msg_is_are} not in that \\
+      range{msg_offenders} {offenders}."
     ))
   }
-
 }
+
 
 
 # Building up on the above, the following function is used within `debit()` and
-# `debit_map()` to check their `x` and `sd` inputs:
-check_debit_inputs <- function(x, sd) {
-  proto_debit_inputs(input = x, type = "mean", symbol = "x")
-  proto_debit_inputs(input = sd, type = "standard deviation", symbol = "sd")
+# `debit_map()` to check the numeric range of their `x` and `sd` inputs:
+check_debit_inputs_all <- function(x, sd) {
+  check_debit_inputs(input = x, type = "mean", symbol = "x")
+  check_debit_inputs(input = sd, type = "standard deviation", symbol = "sd")
 }
+
 
 
 
@@ -49,7 +56,8 @@ check_debit_inputs <- function(x, sd) {
 
 #' @include utils.R decimal-places.R sd-binary.R round.R unround.R reround.R
 
-debit_table <- function(x, sd, n, group_0 = NA, group_1 = NA,
+debit_table <- function(x, sd, n, unround_x = TRUE, unround_sd = TRUE,
+                        group_0 = NA, group_1 = NA,
                         formula = "mean_n", rounding = "up_or_down",
                         threshold = 5, symmetric = FALSE, show_rec = TRUE) {
 
@@ -57,19 +65,22 @@ debit_table <- function(x, sd, n, group_0 = NA, group_1 = NA,
 
   # As trailing zeros matter for DEBIT, mean and standard deviation need to be
   # given as strings:
-  if (!is.character(x)) {
-    cli::cli_abort(c(
-      "`x` is {an_a_type(x)}.",
-      "i" = "It needs to be a string."
-    ))
-  }
+  # check_type(x,  "character")
+  # check_type(sd, "character")
 
-  if (!is.character(sd)) {
-    cli::cli_abort(c(
-      "`sd` is {an_a_type(sd)}.",
-      "i" = "It needs to be a string."
-    ))
-  }
+  # if (!is.character(x)) {
+  #   cli::cli_abort(c(
+  #     "`x` is {an_a_type(x)}.",
+  #     "i" = "It needs to be a string."
+  #   ))
+  # }
+  #
+  # if (!is.character(sd)) {
+  #   cli::cli_abort(c(
+  #     "`sd` is {an_a_type(sd)}.",
+  #     "i" = "It needs to be a string."
+  #   ))
+  # }
 
 
   # Main part ---
@@ -98,23 +109,58 @@ debit_table <- function(x, sd, n, group_0 = NA, group_1 = NA,
   # Recover lower and upper bounds for the original SD and mean values, going by
   # the reported value each...
   sd_unrounded <- suppressMessages(unround(
-    sd_chr, rounding = rounding, digits = digits_sd
+    sd_chr[unround_sd], rounding = rounding, digits = digits_sd
   ))
 
   x_unrounded  <- suppressMessages(unround(
-    x_chr, rounding = rounding, digits = digits_x
+    x_chr[unround_x], rounding = rounding, digits = digits_x
   ))
 
   # ...and define values out of the tibble resulting from that call:
-  sd_upper <- sd_unrounded$upper
-  sd_lower <- sd_unrounded$lower
-  sd_incl_lower <- sd_unrounded$incl_lower
-  sd_incl_upper <- sd_unrounded$incl_upper
+  # sd_upper <- sd_unrounded$upper
+  # sd_lower <- sd_unrounded$lower
+  # sd_incl_lower <- sd_unrounded$incl_lower
+  # sd_incl_upper <- sd_unrounded$incl_upper
+  #
+  # x_upper <- x_unrounded$upper
+  # x_lower <- x_unrounded$lower
+  # x_incl_upper <- x_unrounded$incl_upper
+  # x_incl_lower <- x_unrounded$incl_lower
 
-  x_upper <- x_unrounded$upper
-  x_lower <- x_unrounded$lower
-  x_incl_upper <- x_unrounded$incl_upper
-  x_incl_lower <- x_unrounded$incl_lower
+  # x_upper       <- dplyr::if_else(unround_x,   x_unrounded$upper, x)
+  # x_lower       <- dplyr::if_else(unround_x,   x_unrounded$lower, x)
+  # x_incl_lower  <- dplyr::if_else(unround_x,   x_unrounded$incl_lower, TRUE)
+  # x_incl_upper  <- dplyr::if_else(unround_x,   x_unrounded$incl_upper, TRUE)
+  #
+  # sd_upper      <- dplyr::if_else(unround_sd, sd_unrounded$upper, sd)
+  # sd_lower      <- dplyr::if_else(unround_sd, sd_unrounded$lower, sd)
+  # sd_incl_lower <- dplyr::if_else(unround_sd, sd_unrounded$incl_lower, TRUE)
+  # sd_incl_upper <- dplyr::if_else(unround_sd, sd_unrounded$incl_upper, TRUE)
+
+
+  if (unround_x) {
+    x_upper       <- x_unrounded$upper
+    x_lower       <- x_unrounded$lower
+    x_incl_lower  <- x_unrounded$incl_lower
+    x_incl_upper  <- x_unrounded$incl_upper
+  } else {
+    x_upper       <- x
+    x_lower       <- x
+    x_incl_lower  <- TRUE
+    x_incl_upper  <- TRUE
+  }
+
+  if (unround_sd) {
+    sd_upper      <- sd_unrounded$upper
+    sd_lower      <- sd_unrounded$lower
+    sd_incl_lower <- sd_unrounded$incl_lower
+    sd_incl_upper <- sd_unrounded$incl_upper
+  } else {
+    sd_upper      <- sd
+    sd_lower      <- sd
+    sd_incl_lower <- TRUE
+    sd_incl_upper <- TRUE
+  }
 
 
   # Reconstruct the original SD:
@@ -149,14 +195,22 @@ debit_table <- function(x, sd, n, group_0 = NA, group_1 = NA,
 
   sd_rec_both <- c(sd_rec_lower, sd_rec_upper)
 
-  consistency <- dplyr::case_when(
+  # consistency <- dplyr::case_when(
+  #   sd_incl_lower  &&  sd_incl_upper   ~  any(sd_lower <= sd_rec_both) && any(sd_rec_both <= sd_upper),
+  #   sd_incl_lower  && !sd_incl_upper   ~  any(sd_lower <= sd_rec_both) && any(sd_rec_both <  sd_upper),
+  #   !sd_incl_lower &&  sd_incl_upper   ~  any(sd_lower <  sd_rec_both) && any(sd_rec_both <= sd_upper),
+  #   TRUE                               ~  any(sd_lower <  sd_rec_both) && any(sd_rec_both <  sd_upper)
+  # )
 
-    sd_incl_lower &&  sd_incl_upper    ~  any(sd_lower <= sd_rec_both) && any(sd_rec_both <= sd_upper),
-    sd_incl_lower && !sd_incl_upper    ~  any(sd_lower <= sd_rec_both) && any(sd_rec_both <  sd_upper),
-    !sd_incl_lower &&  sd_incl_upper   ~  any(sd_lower <  sd_rec_both) && any(sd_rec_both <= sd_upper),
-    TRUE                               ~  any(sd_lower <  sd_rec_both) && any(sd_rec_both <  sd_upper)
-
-  )
+  if (sd_incl_lower && sd_incl_upper) {
+    consistency <- any(sd_lower <= sd_rec_both) && any(sd_rec_both <= sd_upper)
+  } else if (sd_incl_lower && !sd_incl_upper) {
+    consistency <- any(sd_lower <= sd_rec_both) && any(sd_rec_both < sd_upper)
+  } else if (!sd_incl_lower && sd_incl_upper) {
+    consistency <- any(sd_lower < sd_rec_both)  && any(sd_rec_both  <= sd_upper)
+  } else {
+    consistency <- any(sd_lower < sd_rec_both)  && any(sd_rec_both  < sd_upper)
+  }
 
   # `n` is always a whole number, so it's good to convert it to integer:
   n <- as.integer(n)
@@ -166,11 +220,15 @@ debit_table <- function(x, sd, n, group_0 = NA, group_1 = NA,
   # (rounding method, boundary values, and Boolean information about the
   # boundary values being inclusive or not):
   if (show_rec) {
-    tibble::tibble(sd = sd_chr, x = x_chr, n, consistency, rounding,
-                   sd_lower, sd_incl_lower, sd_incl_upper, sd_upper,
-                   x_lower, x_incl_lower, x_upper, x_incl_upper)
+    out <- tibble::tibble(
+      sd = sd_chr, x = x_chr, n, consistency, rounding,
+      sd_lower, sd_incl_lower, sd_incl_upper, sd_upper,
+      x_lower, x_incl_lower, x_upper, x_incl_upper
+    )
+    return(out)
   } else {
-    tibble::tibble(sd = sd_chr, x = x_chr, n, consistency)
+    out <- tibble::tibble(sd = sd_chr, x = x_chr, n, consistency)
+    return(out)
   }
 
 }
