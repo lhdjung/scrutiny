63b6f071eec2067fb0fd27400333ae3ea1beb617
Lukas Jung <jung-lukas@gmx.net>
2022-08-06 17:12:28 +0200

    add more factory helpers

--- R/function-factory-helpers.R
+++ R/function-factory-helpers.R
@@ -137,6 +137,16 @@ check_factory_dots <- function(fun, fun_name_scalar, ...) {
 
 
 
+# Get an `arg_list` object; i.e., a named list of arguments passed by the user
+# who called function within which `call_arg_list()` was called:
+call_arg_list <- function() {
+  out <- as.list(rlang::caller_call())
+  out[-(1:2)]
+}
+
+
+
+
 # Helper for the epilogue of function factories (i.e., the part after the first
 # version of the factory-made function is created). Insert parameters named
 # after the key columns into `fun()`, with `NULL` as the default for each.
@@ -205,5 +215,175 @@ absorb_key_args <- function(data, reported, key_cols_call) {
   data <- dplyr::bind_cols(data_renamed, data_not_renamed)
   return(data)
 }
+
+
+
+
+# Much like `absorb_key_args()`, but for any other arguments, and to be
+# reassigned to `fun`, not to `data`:
+absorb_other_args <- function(fun, reported) {
+
+  args_excluded <- c("data", "...", reported)
+
+  arg_list <- formals(fun)
+  arg_list <- arg_list[!(names(arg_list)) %in% args_excluded]
+
+  # Capture any arguments specified by the user of the factory-made function:
+  call_args <- as.list(rlang::caller_call())
+  call_args <- call_args[names(call_args) != ""]
+
+  # Prioritize mapper function defaults before scalar defaults...
+  args_default <- formals(fun)[names(formals(fun)) %in% names(arg_list)]
+  args_default <- names(args_default)
+
+  formals(fun)[args_default] <- arg_list[args_default]
+
+  # ... and user-specified arguments before mapper defaults:
+  formals(fun)[names(formals(fun)) %in% names(call_args)] <-
+    call_args[names(call_args) %in% names(formals(fun))]
+
+  return(fun)
+}
+
+
+
+
+check_args_disabled <- function(args_disabled) {
+
+  # Enforce argument disabling via `args_disabled`:
+  if (!is.null(args_disabled)) {
+    arg_names_caller_call <- rlang::frame_call(frame = parent.frame())
+    arg_names_caller_call <- names(arg_names_caller_call)
+    offenders <- args_disabled[args_disabled %in% arg_names_caller_call]
+    if (length(offenders) > 0) {
+      fun_name <- name_caller_call(n = 2)
+      fun_name_bare <- name_caller_call(wrap = FALSE)
+      fun_name_bare <- as.character(fun_name_bare)
+      package_name <- utils::getAnywhere(fun_name_bare)$where
+      package_name <- as.character(package_name[1])
+      package_name <- sub("package:", "", package_name)
+      if (length(offenders) > 3) {
+        offenders <- offenders[1:3]
+        msg_among_others <- ", among others"
+      } else {
+        msg_among_others <- ""
+      }
+      if (length(offenders) > 1) {
+        msg_arg_s <- "Arguments"
+        msg_is_are <- "are"
+      } else {
+        msg_arg_s <- "Argument"
+        msg_is_are <- "is"
+      }
+      offenders <- wrap_in_backticks(offenders)
+      cli::cli_abort(c(
+        "{msg_arg_s} {offenders} {msg_is_are} \\
+          disabled in {fun_name}{msg_among_others}.",
+        "i" = "This is by design. When {fun_name} was created \\
+          within {package_name} using `scrutiny::function_map()`, \\
+          this function factory's `.args_disabled` argument was \\
+          specified so as to include {offenders}.",
+        "i" = "The purpose is to prevent hidden errors that \\
+          might otherwise arise due to certain arguments not \\
+          working properly within `scrutiny::function_map()`."
+      ))
+    }
+  }
+
+}
+
+
+
+
+# Call this in the prologue to check if the function factory's (!) user
+# specified `.args_disabled` correctly, if at all:
+check_args_disabled_unnamed <- function(args_disabled) {
+  if (!is.null(names(args_disabled))) {
+    name <- deparse(substitute(args_disabled))
+    name_names <- wrap_in_backticks(names(args_disabled))
+    name_fun <- name_caller_call(n = 2)
+    if (length(name_names) == 1) {
+      msg_names <- "this name"
+    } else {
+      msg_names <- "these names"
+    }
+    cli::cli_abort(c(
+      "In {name_fun}, the `{name}` argument must be \\
+      an unnamed string vector.",
+      "x" = "It has {msg_names}: {name_names}."
+    ))
+  }
+}
+
+
+
+
+# Loop through a string vector, `contains`, and check if any of its values is
+# present within any of those of another string vector, the classes of `data`.
+# Classes are ordered by number of characters before the looping, such that the
+# "longest" classes come first by default (`order_decreasing = TRUE`). The first
+# class that fits the first `contains` value is returned; if the first
+# `contains` value doesn't fit any of the classes, the second one is checked,
+# etc. All of this makes sense because scrutiny's classes differ in complexity
+# (e.g., `"scr_grim_map_seq"` versus `"scr_map_seq"`), and the purpose here is
+# to find the most complex class with the earliest in line of the `contains`
+# strings. The latter should be ordered in descending order of desirability, so
+# that the most "desired" string comes first.
+class_with <- function(data, contains, all_classes = FALSE,
+                       order_decreasing = TRUE) {
+  cd <- class(data)
+
+  if (all_classes) {
+    if (length(contains) > 1) {
+      contains <- wrap_in_backticks(contains)
+      cli::cli_abort(c(
+        "`contains` has length {length(contains)}.",
+        "With `all_classes` set to `TRUE`, `contains` \\
+        cannot be longer than 1.",
+        "i" = "(Its values are {contains}.)"
+      ))
+    }
+    out <- cd[stringr::str_detect(cd, contains)]
+    if (length(out) == 0) {
+      cli::cli_abort("No class containing `{contains}` found.")
+    }
+    return(out)
+  }
+
+  cd_lengths <- purrr::map_int(cd, stringr::str_length)
+  cd <- cd[order(cd_lengths, decreasing = order_decreasing)]
+
+  # Outer loop:
+  for (i in 1:length(contains)) {
+
+    # Inner loop:
+    for (j in 1:length(cd)) {
+      cd_contains_string <- stringr::str_detect(cd[j], contains[i])
+      if (cd_contains_string) {
+        return(cd[j])
+      }
+    }
+    # End of inner loop
+
+  }
+  # End of outer loop
+
+
+  # Error if nothing was found:
+  if (length(contains) == 1) {
+    msg_substring_s <- "the substring"
+  } else {
+    msg_substring_s <- " any of these substrings:"
+  }
+  cd <- wrap_in_backticks(cd)
+  contains <- wrap_in_backticks(contains)
+  cli::cli_abort(c(
+    "String not found in classes.",
+    "x" = "`data` inherits no classes containing \\
+    {msg_substring_s} {contains}.",
+    "i" = "It does inherit these classes: {cd}."
+  ))
+}
+
 
 
