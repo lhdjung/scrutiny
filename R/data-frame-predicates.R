223f9a3ae32b5df2a5e18735637fca9c74680b7c
Lukas Jung <jung-lukas@gmx.net>
2023-01-02 03:35:06 +0100

    rework `is_map_df()` docs

--- R/data-frame-predicates.R
+++ R/data-frame-predicates.R
@@ -3,20 +3,31 @@
 #'
 #' @description
 #' - `is_map_df()` tests whether an object is the output of a scrutiny-style
-#' mapper function for consistency tests, like `grim_map()`. These functions
-#' also include those produced by `function_map()`, `function_map_seq()`, and
-#' `function_map_total_n()`.
-#' - `is_map_basic_df()` is a variant of `is_map_df()` that tests if an object
-#' is the output of a "basic" mapper function. This does not include functions
+#' mapper function for consistency tests, like `grim_map()`. These mapper
+#' functions also include those produced by `function_map()`,
+#' `function_map_seq()`, and `function_map_total_n()`.
+#' - `is_map_basic_df()` is a variant of `is_map_df()` that tests whether an
+#' object is the output of a "basic" mapper function. This includes functions
+#' like `grim_map()` and those produced by `function_map()`, but not those
 #' produced by `function_map_seq()` or `function_map_total_n()`.
 #' - `is_map_seq_df()` tests whether an object is the output of a function that
-#' was made by `function_map_seq()`.
+#' was produced by `function_map_seq()`.
 #' - `is_map_total_n_df()` tests whether an object is the output of a function
-#' that was made by `function_map_total_n()`.
+#' that was produced by `function_map_total_n()`.
 #'
 #' @param x Object to be tested.
 #'
 #' @return Boolean (length 1).
+#'
+#' @details Sections 3, 6, and 7 of `vignette("consistency-tests")` discuss
+#'   which function factories produce which functions, and which of these new,
+#'   factory-made functions return which kinds of tibbles.
+#'
+#'   These tibbles are what the `is_map_*()` functions test for. As an example,
+#'   `function_map_seq()` produces `grim_map_seq()`, and this new function
+#'   returns a tibble. `is_map_df()` and `is_map_seq_df()` return `TRUE` for
+#'   this tibble, but `is_map_basic_df()` and `is_map_total_n_df()` return
+#'   `FALSE`.
 #'
 #' @export
 #'
