9f2ed502784794e7639c7b5de4e999a10acc55e3
Lukas Jung <jung-lukas@gmx.net>
2026-04-07 16:28:41 +0200

    Change string columns in `pigs*` data to numeric

--- R/data-doc.R
+++ R/data-doc.R
@@ -10,7 +10,7 @@
 #'
 #' @format A tibble (data frame) with 12 rows and 2 columns. The columns are:
 #' \describe{
-#'  \item{x}{String. Means.}
+#'  \item{x}{Numeric. Means.}
 #'  \item{n}{Numeric. Sample sizes.}
 #' }
 
@@ -36,7 +36,7 @@
 #'
 #' @format A tibble (data frame) with 6 rows and 2 columns. The columns are:
 #' \describe{
-#'  \item{x}{String. Percentages.}
+#'  \item{x}{Numeric. Percentages.}
 #'  \item{n}{Numeric. Sample sizes.}
 #' }
 
@@ -61,8 +61,8 @@
 #'
 #' @format A tibble (data frame) with 7 rows and 3 columns. The columns are:
 #' \describe{
-#'  \item{x}{String. Means.}
-#'  \item{sd}{String. Standard deviations.}
+#'  \item{x}{Numeric. Means.}
+#'  \item{sd}{Numeric. Standard deviations.}
 #'  \item{n}{Numeric. Sample sizes.}
 #' }
 
@@ -89,9 +89,9 @@
 #' @format A tibble (data frame) with 5 rows and 3 columns, describing various
 #'   body measures of the fictional pigs. The columns are:
 #' \describe{
-#'  \item{snout}{String. Snout width.}
-#'  \item{tail}{String. Tail length.}
-#'  \item{wings}{String. Wingspan.}
+#'  \item{snout}{Numeric. Snout width.}
+#'  \item{tail}{Numeric. Tail length.}
+#'  \item{wings}{Numeric. Wingspan.}
 #' }
 
 #' @return A tibble (data frame).
@@ -112,8 +112,8 @@
 #'
 #' @format A tibble (data frame) with 12 rows and 3 columns. The columns are:
 #' \describe{
-#'  \item{x}{String. Means.}
-#'  \item{sd}{String. Standard deviations.}
+#'  \item{x}{Numeric. Means.}
+#'  \item{sd}{Numeric. Standard deviations.}
 #'  \item{n}{Numeric. Sample sizes.}
 #' }
 
@@ -125,78 +125,81 @@
 #'
 "pigs5"
 
-# # Code for creating the data (normally outcommented) --------------------
+# Code for creating the data (normally outcommented) --------------------
 
 # # GRIM for means:
+# # fmt: skip
 # pigs1 <- tibble::tribble(
-#   ~x,       ~n,
-#   "7.22",    32,
-#   "4.74",    25,
-#   "5.23",    29,
-#   "2.57",    24,
-#   "6.77",    27,
-#   "2.68",    28,
-#   "7.01",    29,
-#   "7.38",    26,
-#   "3.14",    27,
-#   "6.89",    31,
-#   "5.00",    25,
-#   "0.24",    28,
+#   ~x,      ~n,
+#   7.22,    32,
+#   4.74,    25,
+#   5.23,    29,
+#   2.57,    24,
+#   6.77,    27,
+#   2.68,    28,
+#   7.01,    29,
+#   7.38,    26,
+#   3.14,    27,
+#   6.89,    31,
+#   5.00,    25,
+#   0.24,    28,
 # )
 #
 #
 # # GRIM for percentages:
+# # fmt: skip
 # pigs2 <- tibble::tribble(
-#   ~x,
-#   "67.4",
-#   "54.2",
-#   "54.0",
-#   "69.8",
-#   "68.1",
-#   "55.4",
-# ) %>%
-#   dplyr::mutate(n = 150)
+#   ~x,   ~n,
+#   67.4, 150,
+#   54.2, 150,
+#   54.0, 150,
+#   69.8, 150,
+#   68.1, 150,
+#   55.4, 150,
+# )
 #
 #
 # # DEBIT:
+# # fmt: skip
 # pigs3 <- tibble::tribble(
-#   ~x,         ~sd,
-#   "0.53",     "0.50",
-#   "0.44",     "0.50",
-#   "0.77",     "0.42",
-#   "0.19",     "0.35",
-#   "0.34",     "0.47",
-#   "0.93",     "0.25",
-#   "0.12",     "0.33"
-# ) %>%
-#   dplyr::mutate(n = 1683)
+#   ~x,       ~sd,    ~n,
+#   0.53,     0.50,   1683,
+#   0.44,     0.50,   1683,
+#   0.77,     0.42,   1683,
+#   0.19,     0.35,   1683,
+#   0.34,     0.47,   1683,
+#   0.93,     0.25,   1683,
+#   0.12,     0.33,   1683,
+# )
 #
 #
+# # fmt: skip
 # pigs4 <- tibble::tribble(
-#   ~snout,  ~tail,    ~wings,
-#   "4.73",   "6.88",  "6.09",
-#   "8.13",   "7.33",  "8.27",
-#   "4.22",   "5.17",  "4.40",
-#   "4.22",   "7.57",  "5.92",
-#   "5.17",   "8.13",  "5.17"
+#   ~snout,  ~tail,  ~wings,
+#   4.73,    6.88,   6.09,
+#   8.13,    7.33,   8.27,
+#   4.22,    5.17,   4.40,
+#   4.22,    7.57,   5.92,
+#   5.17,    8.13,   5.17,
 # )
 #
 #
 # # GRIMMER:
+# # fmt: skip
 # pigs5 <- tibble::tribble(
 #   ~x,      ~sd,     ~n,
-#   "7.22",  "5.30",  38,
-#   "4.74",  "6.55",  31,
-#   "5.23",  "2.55",  35,
-#   "2.57",  "2.57",  30,
-#   "6.77",  "2.18",  33,
-#   "2.68",  "2.59",  34,
-#   "7.01",  "6.68",  35,
-#   "7.38",  "3.65",  32,
-#   "3.14",  "5.32",  33,
-#   "6.89",  "4.18",  37,
-#   "5.00",  "2.18",  31,
-#   "0.24",  "6.43",  34
+#   7.22,    5.30,    38,
+#   4.74,    6.55,    31,
+#   5.23,    2.55,    35,
+#   2.57,    2.57,    30,
+#   6.77,    2.18,    33,
+#   2.68,    2.59,    34,
+#   7.01,    6.68,    35,
+#   7.38,    3.65,    32,
+#   3.14,    5.32,    33,
+#   6.89,    4.18,    37,
+#   5.00,    2.18,    31,
+#   0.24,    6.43,    34,
 # )
 #
 #
